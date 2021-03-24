d55a8b5cf715c9fc899b78e3743e8b200b268635
michelleroos <hello.michelleroos@gmail.com>
2021-03-24 17:06:37 -0400

    Continued working on import_db.sql

--- import_db.sql
+++ import_db.sql
@@ -1,42 +1,60 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    f_name VARCHAR(255) NOT NULL, 
-    l_name VARCHAR(255) NOT NULL,
     id INTEGER PRIMARY KEY,
+    f_name VARCHAR(255) NOT NULL, 
+    l_name VARCHAR(255) NOT NULL
 );
-
-DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions (
-    title TEXT NOT NULL,
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255) NOT NULL, 
     body TEXT NOT NULL,
-    user_id INTEGER NOT NULL, 
-    question_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(author_id) REFERENCES users(id)
 );
-
-DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
-    question TEXT NOT NULL,
     user_id INTEGER NOT NULL, 
-    FOREIGN KEY(user_id) REFERENCES users(id)
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
-);
 
-DROP TABLE IF EXISTS replies;
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
 
 CREATE TABLE replies ( 
-    parent TEXT NOT NULL, 
+    id INTEGER PRIMARY KEY,
+    parent TEXT,
     reply TEXT NOT NULL,
-    user_id  INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    question TEXT NOT NULL,
     question_id  TEXT NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id)
-    FOREIGN KEY(parent) REFERENCES questions(question_id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(reply) REFERENCES replies(parent),
+    FOREIGN KEY(parent) REFERENCES question(question_id)
 );
+
+CREATE TABLE question_likes (
+    question_id TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(question_id) REFERENCES questions(question_id)
+);
+
+INSERT INTO
+    users (f_name, l_name)
+VALUES
+    ('Elvin', 'Marseilles'),
+    ('Michelle', 'Roos'),
+    ('Steve', 'Jackson'),
+    ('Brian', 'George');
